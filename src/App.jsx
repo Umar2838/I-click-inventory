8060400b864311cf1150eabdac69c5036e7a7370
Umar Aqeel <112412509+Umar2838@users.noreply.github.com>
2024-03-05 02:25:41 +0500

    started

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Login from './components/Login/Login'
+function App() {
+
+  return (
+    <div className='app'>
+    <Login />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Login.css"
+const Login = () => {
+  return (
+    <div className='login-container'>
+    <div className='wrapper' >
+<div className='brand-div' >
+  <h1>I-Click Inventory</h1>
+</div>
+<div className='form-div' ></div>
+    </div>
+    </div>
+  )
+}
+
+export default Login
