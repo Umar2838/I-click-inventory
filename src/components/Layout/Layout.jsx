c827a8098fc92b4c1d7a0220109e4a0854e909b2
Umar Aqeel <112412509+Umar2838@users.noreply.github.com>
2024-03-18 02:34:48 +0500

    new order page design

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -15,7 +15,14 @@ import { FaEye } from "react-icons/fa";
 import {Chart as ChartJS} from "chart.js/auto"
 import {Bar} from "react-chartjs-2"
 import { DataGrid } from '@mui/x-data-grid';
-
+import { Button, Modal } from 'antd';
+import {
+  DatePicker,
+  Form,
+  Input,
+  InputNumber,
+  Radio,
+} from 'antd';
 
 const headerStyle = {
   textAlign: 'center',
@@ -29,9 +36,8 @@ const headerStyle = {
   };
 const contentStyle = {
   textAlign: 'center',
+  color:"#000",
   minHeight: 120,
-  lineHeight: '120px',
-  color: '#fff',
   backgroundColor: '#f5f5f5'
 };
 const siderStyle = {
@@ -66,10 +72,14 @@ const Layoutstyle = () => {
     setActiveDiv(divName);
   };
 
+  // view order table
+
   const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
     { field: 'date', headerName: 'Date/Time', width: 130 },
     { field: 'Name', headerName: 'Customer Name', width: 130 },
+    { field: 'phone', headerName: 'Phone.No', width: 130 },
+
     { field: 'frame', headerName: 'Frame', width: 130 },
     {
       field: 'left',
@@ -90,18 +100,61 @@ const Layoutstyle = () => {
   ];
   
   const rows = [
-    { id: 1, date: '01-Dec-24',Name: 'Jon',frame:'sunglass',  left: 35,right:10 ,payment:10000},
-    { id: 2, date: '01-Dec-24', Name: 'Cersei',frame:'sunglass', left: 42,right:10,payment:10000 },
-    { id: 3, date: '01-Dec-24', Name: 'Jaime',frame:'sunglass', left: 45,right:10,payment:10000 },
-    { id: 4, date: '01-Dec-24',Name: 'Arya',frame:'sunglass', left: 16,right:10,payment:10000 },
-    { id: 5, date: '01-Dec-24', Name: 'Daenerys',frame:'sunglass', left: 13 ,right:10,payment:10000 },
-    { id: 6, date: '01-Dec-24', Name: 'umar',frame:'sunglass', left: 15,right:10,payment:10000 },
-    { id: 7, date: '01-Dec-24', Name: 'Ferrara',frame:'sunglass', left: 44,right:1 ,payment:10000},
-    { id: 8, date: '01-Dec-24', Name: 'Rossini',frame:'sunglass', left: 36,right:10,payment:10000 },
-    { id: 9, date: '01-Dec-24',Name: 'Harvey',frame:'sunglass', left: 65,right:10,payment:10000 },
+    { id: 1, date: '01-Dec-24',Name: 'Jon', phone:1234567890, frame:'sunglass',  left: 35,right:10 ,payment:10000},
+    { id: 2, date: '01-Dec-24',Name: 'Cersei',phone:1234567890,frame:'sunglass', left: 42,right:10,payment:10000 },
+    { id: 3, date: '01-Dec-24',Name: 'Jaime',phone:1234567890,frame:'sunglass', left: 45,right:10,payment:10000 },
+    { id: 4, date: '01-Dec-24',Name: 'Arya',phone:1234567890,frame:'sunglass', left: 16,right:10,payment:10000 },
+    { id: 5, date: '01-Dec-24',Name: 'Daenerys',phone:1234567890,frame:'sunglass', left: 13 ,right:10,payment:10000 },
+    { id: 6, date: '01-Dec-24',Name: 'umar',phone:1234567890,frame:'sunglass', left: 15,right:10,payment:10000 },
+    { id: 7, date: '01-Dec-24',Name: 'Ferrara',phone:1234567890,frame:'sunglass', left: 44,right:1 ,payment:10000},
+    { id: 8, date: '01-Dec-24',Name: 'Rossini',phone:1234567890,frame:'sunglass', left: 36,right:10,payment:10000 },
+    { id: 9, date: '01-Dec-24',Name: 'Harvey',phone:1234567890,frame:'sunglass', left: 65,right:10,payment:10000 },
   ];
-  
 
+  // new order modal
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  }
+
+// new order form 
+
+const { RangePicker } = DatePicker;
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 6,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 14,
+    },
+  },
+};
+// const [balance , setBalance] = useState(null)
+// const [total,setTotal] = useState(null)
+// const [advance,setAdvance] = useState(null)
+// setBalance(total - advance);
+
+const onFinish = (values) => {
+  console.log('Success:', values);  
+};
+const onFinishFailed = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
   return(
 
     <Flex gap="middle" wrap="wrap">
@@ -216,6 +269,167 @@ data={{
  
        </div>
        : ""
+}
+
+{  activeDiv ==='newOrder' ? 
+<>
+<div className='order-header' >New Orders</div>
+<Button type="primary" className='orderbtn' onClick={showModal}>
+        Take order
+      </Button>
+      <Modal title="Basic Modal"  open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+      <Form
+    {...formItemLayout}
+    variant="filled"
+    style={{
+      maxWidth: 600,
+    }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <Form.Item
+      label="Customer Name"
+      name="name"
+      rules={[
+        {
+          required: true,
+          message: 'Please input customer name!',
+        },
+      ]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item
+      label="Phone.No"
+      name="phone"
+      rules={[
+        {
+          required: true,
+          message: 'Please input number',
+        },
+      ]}
+    >
+      <InputNumber
+        style={{
+          width: '100%',
+        }}
+      />
+    </Form.Item>
+
+    <Form.Item
+      label="Service Type"
+      name="type"
+      rules={[
+        {
+          required: true,
+          message: 'Please input!',
+        },
+      ]}
+    >
+    <Input/>
+    </Form.Item>
+
+    <Form.Item
+      label="Left Eye "
+      name="leye"
+      rules={[
+        {
+          required: true,
+          message: 'Please input!',
+        },
+      ]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item
+      label="Right Eye"
+      name="reye"
+      rules={[
+        {
+          required: true,
+          message: 'Please input!',
+        },
+      ]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item
+      label="Total"
+      name="total"
+      rules={[
+        {
+          required: true,
+          message: 'Please input!',
+        },
+      ]}
+    >
+      <InputNumber   />
+    </Form.Item>
+
+    <Form.Item
+      label="Advance"
+      name="advance"
+      rules={[
+        {
+          required: true,
+          message: 'Please input!',
+        },
+      ]}
+    >
+      <InputNumber  />
+    </Form.Item>
+
+    <Form.Item
+      label="Balance"
+      name="balance"
+      // rules={[
+      //   {
+      //     required: true,
+      //     message: 'Please input!',
+      //   },
+      // ]}
+    >
+      <InputNumber value={}   readOnly={true} />
+    </Form.Item>
+    <Form.Item
+      label="DatePicker"
+      name="DatePicker"
+      rules={[
+        {
+          required: true,
+          message: 'Please input!',
+        },
+      ]}
+    >
+      <DatePicker />
+    </Form.Item>
+    <Form.Item label="Status" name="status">
+          <Radio.Group>
+            <Radio value="delivered"> Delivered </Radio>
+            <Radio value="pending"> Pending </Radio>
+          </Radio.Group>
+        </Form.Item>
+    <Form.Item
+      wrapperCol={{
+        offset: 6,
+        span: 16,
+      }}
+    >
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
+      </Modal>
+
+
+</>
+: "" 
+
 }
         </Content>
         <Footer style={footerStyle}>copyright© {new Date().getFullYear()} I-Click Optics POS software </Footer>
